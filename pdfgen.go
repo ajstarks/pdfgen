7f83cc380ed6afbab20a504ef54df3c86ac19e4a
Anthony Starks <ajstarks@gmail.com>
2018-06-12 19:10:27 -0400

    fix multiple page logic, add function comments

--- pdfgen.go
+++ pdfgen.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 )
 
+// PDFDoc defines the document structure
 type PDFDoc struct {
 	Writer        io.Writer
 	width, height float64
@@ -16,6 +17,21 @@ type PDFDoc struct {
 
 var fontmap = map[string]string{"sans": "Helvetica", "serif": "Times-Roman", "mono": "Courier", "symbol": "Zapf-Dingbats"}
 
+const (
+	rectfmt    = "%s rg %v %v %v %v re f\n"
+	linefmt    = "%v w %s RG %v %v m %v %v l S\n"
+	curvefmt   = "%v w %s RG %v %v m %v %v %v %v v S\n"
+	endfmt     = "trailer\n<</Size %d /Root 1 0 R >>\n%%%%EOF\n"
+	textfmt    = "BT /%s %v Tf %v %v Td %s rg (%s) Tj ET\n"
+	newpagefmt = "%d 0 obj\n<</Type /Page /Parent 1 0 R /Resources 2 0 R /Contents %d 0 R>>\nendobj\n\n%d 0 obj\n<</Length 0>>\nstream\n"
+	colorfmt   = "%.3f %.3f %.3f"
+	imagefmt   = "<</Type /XObject /Subtype /Image /Width %d /Height %d /ColorSpace /DeviceRGB /BitsPerComponent 8 /Filter /DCTDecode /Length %d>>\n"
+	pagefmt    = "] /Count %d /MediaBox [0 0 %v %v]>>\nendobj\n\n"
+	resfmt     = "2 0 obj\n<< /Font\n"
+	fontfmt    = "<< /%s << /Type /Font /Subtype /Type1 /BaseFont /%s >>\n"
+)
+
+// NewDoc initializes the document structure
 func NewDoc(w io.Writer, pagewidth, pageheight float64) *PDFDoc {
 	return &PDFDoc{
 		Writer:      w,
@@ -26,26 +42,14 @@ func NewDoc(w io.Writer, pagewidth, pageheight float64) *PDFDoc {
 	}
 }
 
-const (
-	rectfmt    = "%s rg %v %v %v %v re f\n"
-	linefmt    = "%v w %s RG %v %v m %v %v l S\n"
-	curvefmt   = "%v w %s RG %v %v m %v %v %v %v v S\n"
-	endfmt     = "trailer\n<</Size %d /Root 1 0 R >>\n%%%%EOF\n"
-	textfmt    = "BT /%s %v Tf %v %v Td %s rg (%s) Tj ET\n"
-	newpagefmt = "%d 0 obj\n<</Type /Page /Parent 1 0 R /Resources 2 0 R /Contents %d 0 R>>\nendobj\n\n%d 0 obj\n<</Length 0>>\nstream\n"
-	colorfmt   = "%.1f %.1f %.1f"
-	imagefmt   = "<</Type /XObject /Subtype /Image /Width %d /Height %d /ColorSpace /DeviceRGB /BitsPerComponent 8 /Filter /DCTDecode /Length %d>>\n"
-	pagefmt    = "] /Count %d /MediaBox [0 0 %v %v]>>\nendobj\n\n"
-	resfmt     = "2 0 obj\n<< /Font\n"
-	fontfmt    = "<< /%s << /Type /Font /Subtype /Type1 /BaseFont /%s >>\n"
-)
-
+// Init begins the document
 func (p *PDFDoc) Init(n int) {
-	fmt.Fprintln(p.Writer, "%PDF-1.4")
+	fmt.Fprintln(p.Writer, "%PDF-1.5")
 	p.root(n)
 	p.resources()
 }
 
+// pdfstring returns an escaped string
 func pdfstring(s string) string {
 	s = strings.Replace(s, "\\", "\\\\", -1)
 	s = strings.Replace(s, "(", "\\(", -1)
@@ -54,8 +58,9 @@ func pdfstring(s string) string {
 	return s
 }
 
+// root defines the document root
 func (p *PDFDoc) root(npages int) {
-	// Object 1 is the root, object 2 is resources).
+	// Object 1 is the root, object 2 is resources.
 	// page references begin at 3, with the contents as the next sequential reference.
 	// For example 3 -> 4, 5 -> 6, etc.
 	fmt.Fprintf(p.Writer, "1 0 obj\n<</Type /Catalog /Pages 3 0 R /Kids [")
@@ -67,6 +72,7 @@ func (p *PDFDoc) root(npages int) {
 	p.objectcount++
 }
 
+// Resources defines page resources: fonts, etc.
 func (p *PDFDoc) resources() {
 	f := p.fontnames[0]
 	fmt.Fprint(p.Writer, resfmt)
@@ -77,34 +83,44 @@ func (p *PDFDoc) resources() {
 	p.objectcount++
 }
 
+// EndPage closes out a page
 func (p *PDFDoc) EndPage() {
 	fmt.Fprintf(p.Writer, "endstream\nendobj\n\n")
 	p.objectcount++
 }
 
+// EndDoc closes out the document
 func (p *PDFDoc) EndDoc() {
 	fmt.Fprintf(p.Writer, endfmt, p.objectcount)
 }
 
+// NewPage sets up a new page
+// page references begin at 3, with the contents as the next sequential reference.
 func (p *PDFDoc) NewPage(n int) {
-	fmt.Fprintf(p.Writer, newpagefmt, n+2, n+3, n+3)
+	obj := (2 * n) + 1
+	ref := obj + 1
+	fmt.Fprintf(p.Writer, newpagefmt, obj, ref, ref)
 	p.objectcount++
 }
 
+// pdfcolor converts a color string to the PDF (RGB) format
 func pdfcolor(color string) string {
 	r, g, b := colorlookup(color)
 	return fmt.Sprintf(colorfmt, float64(r)/255.0, float64(g)/255.0, float64(b)/255.0)
 }
 
+// Text draws attributed (font, size, color) text at a (x,y) location
 func (p *PDFDoc) Text(x, y float64, s, font string, size float64, color string) {
 	fmt.Fprintf(p.Writer, textfmt, fontmap[font], size, x, y, pdfcolor(color), pdfstring(s))
 }
 
+// Image places an image at the (x,y) location
 func (p *PDFDoc) Image(x, y float64, width, height int, name string) {
 	length := 0
 	fmt.Fprintf(p.Writer, imagefmt, width, height, length)
 }
 
+// Polygon draws a colored polygon
 func (p *PDFDoc) Polygon(x []float64, y []float64, color string) {
 	if len(x) != len(y) {
 		return
@@ -116,20 +132,23 @@ func (p *PDFDoc) Polygon(x []float64, y []float64, color string) {
 	fmt.Fprintf(p.Writer, " %v %v l f\n", x[0], y[0])
 }
 
+// Line draws a line with  specified stroke color and width
 func (p *PDFDoc) Line(x1, y1, x2, y2, sw float64, color string) {
 	fmt.Fprintf(p.Writer, linefmt, sw, pdfcolor(color), x1, y1, x2, y2)
+}
+
+// Rect draws a rectangle colored with the upper left at (x,y)
+func (p *PDFDoc) Rect(x, y, w, h float64, color string) {
+	fmt.Fprintf(p.Writer, rectfmt, pdfcolor(color), x, y, w, h)
+}
+
+// Curve draws a cubic Bezier curve at the specified stroke color and width
+func (p *PDFDoc) Curve(x1, y1, x2, y2, x3, y3, sw float64, color string) {
+	fmt.Fprintf(p.Writer, curvefmt, sw, pdfcolor(color), x1, y1, x2, y2, x3, y3)
 }
 
 func (p *PDFDoc) Ellipse(x, y, w, h float64) {
 }
 
-func (p *PDFDoc) Rect(x, y, w, h float64, color string) {
-	fmt.Fprintf(p.Writer, rectfmt, pdfcolor(color), x, y, w, h)
-}
-
 func (p *PDFDoc) Arc(x, y, w, h, a1, a2 float64) {
-}
-
-func (p *PDFDoc) Curve(x1, y1, x2, y2, x3, y3, sw float64, color string) {
-	fmt.Fprintf(p.Writer, curvefmt, sw, pdfcolor(color), x1, y1, x2, y2, x3, y3)
 }
